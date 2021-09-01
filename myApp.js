5ba377168d3f3cf85b23ecdad3d5d195e12cc899
John Albright <John@john-albrights-macbook-pro.local>
2021-08-31 21:14:03 -0500

    Added the package cors

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ var normalizeUrl = require('normalize-url');
 
 require('dotenv').config();
 
+// Enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// So that API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 // Connect to my Mongoose DB Atlas account
 mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
